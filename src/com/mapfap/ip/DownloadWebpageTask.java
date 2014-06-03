3b0c23f626132ddc3f5c87d13207cba5e5220d94
mapfap <mapkabb@gmail.com>
2014-06-03 17:35:45 +0700

    Fetch data and redraw widget

---
+++ src/com/mapfap/ip/DownloadWebpageTask.java
@@ -0,0 +1,91 @@
+package com.mapfap.ip;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+public class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+	
+        private Context context;
+		private int appWidgetId;
+
+		public DownloadWebpageTask(Context context, int appWidgetId) {
+        	this.context= context;
+        	this.appWidgetId = appWidgetId;
+        }
+
+		@Override
+        protected String doInBackground(String... urls) {
+              
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+        	AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        	RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget);
+    		rv.setTextViewText(R.id.month_label, result);
+    		appWidgetManager.updateAppWidget(appWidgetId, rv);
+       }
+        
+     // Given a URL, establishes an HttpUrlConnection and retrieves
+    	// the web page content as a InputStream, which it returns as
+    	// a string.
+    	private String downloadUrl(String myurl) throws IOException {
+    	    InputStream is = null;
+    	    // Only display the first 500 characters of the retrieved
+    	    // web page content.
+    	    int len = 500;
+    	        
+    	    try {
+    	        URL url = new URL(myurl);
+    	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    	        conn.setReadTimeout(10000 /* milliseconds */);
+    	        conn.setConnectTimeout(15000 /* milliseconds */);
+    	        conn.setRequestMethod("GET");
+    	        conn.setDoInput(true);
+    	        // Starts the query
+    	        conn.connect();
+    	        int response = conn.getResponseCode();
+    	        Log.d("dfv", "The response is: " + response);
+    	        is = conn.getInputStream();
+
+    	        // Convert the InputStream into a string
+    	        String contentAsString = readIt(is, len);
+    	        return contentAsString;
+    	        
+    	    // Makes sure that the InputStream is closed after the app is
+    	    // finished using it.
+    	    } finally {
+    	        if (is != null) {
+    	            is.close();
+    	        } 
+    	    }
+    	}
+    	
+    	// Reads an InputStream and converts it to a String.
+    	public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+    	    Reader reader = null;
+    	    reader = new InputStreamReader(stream, "UTF-8");        
+    	    char[] buffer = new char[len];
+    	    reader.read(buffer);
+    	    return new String(buffer);
+    	}
+
+    }
